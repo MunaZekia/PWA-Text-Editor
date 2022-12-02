2a65fe19111f7bc3e80c5bc34458923f6de47371
Joel Spriggs <106184761+joel-spriggs@users.noreply.github.com>
2022-12-01 22:42:43 -0500

    adding solutions for Javascript night 2

--- 01-Class-Content/03-JavaScript/01-Activities/17-Ins_Scope/script.js
+++ 01-Class-Content/03-JavaScript/01-Activities/17-Ins_Scope/script.js
@@ -1,13 +1,13 @@
 // A variable declared in global scope is available to all functions
-var hello = "Hello"; 
+var globalHello = "Hello";
 
 function sayHello() {
-  console.log(hello);
+  console.log("from sayHello " + globalHello);
   return;
 }
 
 var sayHelloAgain = function () {
-  console.log(hello);
+  console.log("from sayHelloAgain " + globalHello);
   return;
 };
 
@@ -15,34 +15,35 @@ sayHello();
 sayHelloAgain();
 
 //A variable declared in local scope is only available to that function
-function sayGoodbye() { 
+function sayGoodbye() {
   var goodbye = "Goodbye";
-  console.log(goodbye);
+  console.log("from sayGoodbye " + goodbye);
   return;
 }
 
 //This will throw an error
-var sayGoodbyeAgain = function () { 
+var sayGoodbyeAgain = function () {
   console.log(goodbye);
   return;
 };
 
 // Shadowing happens when the same variable is used in the local and global scope
 var shadow = "Hello";
- console.log(shadow);
+console.log("global!  " + shadow);
 
 // Logs "Hello"
 function sayWhat() {
-  console.log(shadow);
+  console.log("from sayWhat  " + shadow);
   return;
 }
 
 //Logs "Goodbye"
 var sayWhatAgain = function () {
   var shadow = "Goodbye";
-  console.log(shadow);   
+  console.log("from sayWhatAgain " + shadow);
 };
 
 sayGoodbye();
 sayWhat();
 sayWhatAgain();
+sayWhat();
--- 01-Class-Content/03-JavaScript/01-Activities/19-Ins_Methods/script.js
+++ 01-Class-Content/03-JavaScript/01-Activities/19-Ins_Methods/script.js
@@ -1,23 +1,30 @@
-var comparisonOperators = ["Equal", "Not Equal", "Strict Equal", "Strict Not Equal", "Greater Than", "Less Than"];
+var comparisonOperators = [
+  "Equal",
+  "Not Equal",
+  "Strict Equal",
+  "Strict Not Equal",
+  "Greater Than",
+  "Less Than",
+];
 var arithmaticOperators = ["+", "-", "%"];
 var logicalOperators = ["and", "or", "not"];
 var myString = "Hello String";
 
 //Array Methods
 // Sorts comparisonOperators array and returns the sorted array
-comparisonOperators.sort(); 
+comparisonOperators.sort();
 
 //Logs sorted array
 console.log(comparisonOperators);
 
 // Adds elements to end of an array. Takes in at least one parameter
-arithmaticOperators.push("%");
+arithmaticOperators.push("%", "<>");
 
 //Logs array with element "%" added to end
 console.log(arithmaticOperators);
 
 //Returns selected elements as a new array.
-var logicalOperatorsSliced = logicalOperators.slice(0,2);
+var logicalOperatorsSliced = logicalOperators.slice(0, 2);
 
 //Logs new array
 console.log(logicalOperatorsSliced);
@@ -27,7 +34,7 @@ console.log(logicalOperators);
 
 //String Methods
 //Replaces "String" with "World" and returns new string
-var myNewString = myString.replace("String", "World");
+var myNewString = myString.concat("World");
 console.log(myNewString);
 
 //The orginal string is unchanged
