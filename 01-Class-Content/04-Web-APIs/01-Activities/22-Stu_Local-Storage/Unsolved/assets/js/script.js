739f359bb1332a835f55d0f62949534bc11a619e
Joel Spriggs <106184761+joel-spriggs@users.noreply.github.com>
2022-12-13 09:17:33 -0500

    solutions for module 4 day 3, new activities, algos and challenge for module 5

--- 01-Class-Content/04-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
+++ 01-Class-Content/04-Web-APIs/01-Activities/22-Stu_Local-Storage/Unsolved/assets/js/script.js
@@ -14,9 +14,11 @@ function displayMessage(type, message) {
 
 function renderLastRegistered() {
   // TODO: Retrieve the last email and password and render it to the page
+  userEmailSpan.textContent = localStorage.getItem("lastEmail");
+  userPasswordSpan.textContent = localStorage.getItem("lastPassword");
 }
 
-signUpButton.addEventListener("click", function(event) {
+signUpButton.addEventListener("click", function (event) {
   event.preventDefault();
 
   var email = document.querySelector("#email").value;
@@ -29,6 +31,9 @@ signUpButton.addEventListener("click", function(event) {
   } else {
     displayMessage("success", "Registered successfully");
 
-  // TODO: Save email and password to localStorage and render the last registered user
+    // TODO: Save email and password to localStorage and render the last registered user
+    localStorage.setItem("lastEmail", email);
+    localStorage.setItem("lastPassword", password);
+    renderLastRegistered();
   }
 });
