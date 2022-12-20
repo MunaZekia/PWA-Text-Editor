5a7712c627d2b641ea4ed90f6325aff9743312dc
Joel Spriggs <106184761+joel-spriggs@users.noreply.github.com>
2022-12-19 21:15:20 -0500

    making mini project more challenging

---
+++ 01-Class-Content/05-Third-Party-APIs/01-Activities/28-Stu_Mini-Project/Main/assets/js/script.js
@@ -0,0 +1,7 @@
+// save reference to important DOM elements
+var timeDisplayEl = $("#time-display");
+var projectDisplayEl = $("#project-display");
+var projectFormEl = $("#project-form");
+var projectNameInputEl = $("#project-name-input");
+var projectTypeInputEl = $("#project-type-input");
+var projectDateInputEl = $("#project-date-input");
