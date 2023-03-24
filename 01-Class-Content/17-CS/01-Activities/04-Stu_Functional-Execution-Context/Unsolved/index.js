e858df73a3aa06d12c833b0ed3e5900836d2ded9
Joel Spriggs <joel.spriggs@collibra.com>
2023-03-23 22:38:08 -0400

    solutions to Modules 17.1 & 17.2

--- 01-Class-Content/17-CS/01-Activities/04-Stu_Functional-Execution-Context/Unsolved/index.js
+++ 01-Class-Content/17-CS/01-Activities/04-Stu_Functional-Execution-Context/Unsolved/index.js
@@ -2,12 +2,13 @@
 function avg(array) {
   function sum() {
     let total = 0;
-    for(let i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
       total += array[i];
     }
+    return total;
   }
-  
-  return total/array.length;
+  let total = sum();
+  return sum() / array.length;
 }
 
 module.exports = avg;
