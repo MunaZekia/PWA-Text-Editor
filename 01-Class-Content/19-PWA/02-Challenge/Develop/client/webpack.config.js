58a5518107a06725afce6f8b80231eefe1517c2a
Muna Zeki <munazeki@Munas-MacBook-Pro.local>
2023-05-09 19:16:41 -0500

    fixed database.js file

--- 01-Class-Content/19-PWA/02-Challenge/Develop/client/webpack.config.js
+++ 01-Class-Content/19-PWA/02-Challenge/Develop/client/webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
+
 // TODO: Add CSS loaders and babel to webpack.
 
 
@@ -19,12 +20,35 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'Progressive',
+      }),
     ],
+    experiments: {
+      topLevelAwait: true,
+    },
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test:/\.js$/,
+          // /\.js$/ matches all files ending in .js
+          exclude: /node_modules/,
+
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-class-properties',
+              '@babel/plugin-transform-runtime'],
+            }
+          }
+        }
       ],
     },
   };
